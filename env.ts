bfde231bd5c8b53c57b7b0c14ac2c1f5a365babb
CryRtex <itspecialistt@protonmail.com>
2023-01-29 15:17:37 +0300

    Update env.ts

--- env.ts
+++ env.ts
@@ -1,6 +1,7 @@
 export const EnvVars = {
-  SITE_NAME: 'My SaaS Startup',
-  OG_IMAGES_URL: 'https://next-saas-starter-ashy.vercel.app/og-images/',
-  URL: 'https://next-saas-starter-ashy.vercel.app/',
+  SITE_NAME: 'Selenoid.tech',
+  OG_IMAGES_URL: 'https://res.cloudinary.com/do6z8bo0x/image/upload/v1674992642/selenoid/phox-logo-1_lreoll.png',
+  URL: 'https://selenoid.tech/',
   MAILCHIMP_SUBSCRIBE_URL: 'https://bstefanski.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
 };
+
