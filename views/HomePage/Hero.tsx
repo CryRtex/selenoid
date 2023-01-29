563b9cbd8b61ee9cecb35ad2d6f300f4336a2eb4
CryRtex <itspecialistt@protonmail.com>
2023-01-29 15:39:30 +0300

    Update Hero.tsx

--- views/HomePage/Hero.tsx
+++ views/HomePage/Hero.tsx
@@ -14,11 +14,10 @@ export default function Hero() {
   return (
     <HeroWrapper>
       <Contents>
-        <CustomOverTitle>the coolest, saas product you have ever seen</CustomOverTitle>
-        <Heading>Make your life easier with our SaaS</Heading>
+        <CustomOverTitle>Tamamen ücretsiz sunucu hizmeti</CustomOverTitle>
+        <Heading>Sunucunuzu daha kolay oluşturun</Heading>
         <Description>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, tempora qui. Explicabo voluptate iure ipsum molestias
-          repudiandae perspiciatis nostrum praesentium, unde pariatur tempora magni rem. Necessitatibus facilis obcaecati ratione.
+          Hizmetlerimizden tamamen ücretsiz yararlanabiliyorsunuz. Üstelik istediğiniz kadar kaynak kullanımı ile birlikte
         </Description>
         <CustomButtonGroup>
           <Button onClick={() => setIsModalOpened(true)}>
