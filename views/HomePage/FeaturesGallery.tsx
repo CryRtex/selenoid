d9138300803d0506fc42b0bbec4984a1e52389b5
CryRtex <itspecialistt@protonmail.com>
2023-01-29 16:39:16 +0300

    Update FeaturesGallery.tsx

--- views/HomePage/FeaturesGallery.tsx
+++ views/HomePage/FeaturesGallery.tsx
@@ -10,25 +10,25 @@ import { media } from 'utils/media';
 
 const TABS = [
   {
-    title: 'Find relevant media contacts - multiline title',
+    title: 'Kendine Özgü Sunucunu Oluştur',
     description:
-      '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quidem ipsam ratione dicta quis cupiditate consequuntur laborum ducimus iusto velit.</p>',
+      '<p>Kendine özgü sunucunu tek adımda oluştur ve kullanmaya başla. İşte bu kadar basit</p>',
     imageUrl: '/demo-illustration-3.png',
     baseColor: '249,82,120',
     secondColor: '221,9,57',
   },
   {
-    title: 'Another amazing feature',
+    title: 'Ek Özellikler',
     description:
-      '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quidem ipsam ratione dicta quis cupiditate consequuntur laborum ducimus iusto velit.</p>',
+      '<p>Sunucuna ek özellik eklemek çok basit tek tuşla entegre et ve özellik aktif olsun.</p>',
     imageUrl: '/demo-illustration-4.png',
     baseColor: '57,148,224',
     secondColor: '99,172,232',
   },
   {
-    title: 'And yet... another truly fascinating feature',
+    title: 'Vee Sonunda İşlemcilerimiz Tamamen AMD Ryzen Threadripper',
     description:
-      '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quidem ipsam ratione dicta quis cupiditate consequuntur laborum ducimus iusto velit.</p>',
+      '<p>İşlemcilerimizi tamamen AMD üzerinde konfigüre ediyor ve sizler için en iyi işlemciyi oluşturuyoruz.</p>',
     imageUrl: '/demo-illustration-5.png',
     baseColor: '88,193,132',
     secondColor: '124,207,158',
