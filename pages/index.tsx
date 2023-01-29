ce4e41a76c1bcaf237462c7bf8a41a7cd5f7af4d
CryRtex <itspecialistt@protonmail.com>
2023-01-29 16:09:25 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,25 +27,18 @@ export default function Homepage({ posts }: InferGetStaticPropsType<typeof getSt
         <WhiteBackgroundContainer>
           <Hero />
           <Partners />
-          <BasicSection imageUrl="/demo-illustration-1.svg" title="Lorem ipsum dolor sit amet consectetur." overTitle="sit amet gogo">
+          <BasicSection imageUrl="/demo-illustration-1.svg" title="Kolay Kullanımlı Bir Panel." overTitle="Kontrol Paneli">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, quidem error incidunt a doloremque voluptatem porro inventore
-              voluptate quo deleniti animi laboriosam.{' '}
-              <Link href="/help-center">Possimus ullam velit rem itaque consectetur, in distinctio?</Link> Lorem ipsum, dolor sit amet
-              consectetur adipisicing elit. Soluta repellendus quia quos obcaecati nihil. Laudantium non accusantium, voluptate eum nesciunt
-              at suscipit quis est soluta?
+              Kontrol panellerimiz tamamen ücretsiz ve kolay kullanım için organize edildi..{' '}
+              <Link href="https://selenoid.tech/panel"> Eğer ki yardıma yada herhangi bir şeye ihtiyacın varsa burada olacağız.
             </p>
           </BasicSection>
-          <BasicSection imageUrl="/demo-illustration-2.svg" title="Lorem ipsum dolor sit." overTitle="lorem ipsum" reversed>
+          <BasicSection imageUrl="/demo-illustration-2.svg" title="Sunucu saati ve ek özellikler." overTitle="Nasıl Kazanırım" reversed>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, quidem error incidunt a doloremque voluptatem porro inventore{' '}
-              <strong>voluptate quo deleniti animi laboriosam</strong>. Possimus ullam velit rem itaque consectetur, in distinctio?
+              Sunucu saati kazanmak yada ek özellikler mi istiyorsun bize panelden bir bilet açman yeterli olacaktır{' '}
+              <strong>Daha yüksek kaynaklara mı ihtiyacın var?
             </p>
-            <ul>
-              <li>Professional point 1</li>
-              <li>Professional remark 2</li>
-              <li>Professional feature 3</li>
-            </ul>
+            
           </BasicSection>
         </WhiteBackgroundContainer>
         <DarkerBackgroundContainer>
