0157c3ca5aee744695c85dbe2ea6ed404afd1253
CryRtex <itspecialistt@protonmail.com>
2023-01-29 17:43:35 +0300

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo({ ...rest }) {
   return (
       <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-       width="393.000000pt" height="220.000000pt" viewBox="0 0 393.000000 220.000000"
+       width="300.000000pt" height="180.000000pt" viewBox="0 0 300.000000 180.000000"
        preserveAspectRatio="xMidYMid meet">
 
       <g transform="translate(0.000000,220.000000) scale(0.100000,-0.100000)"
