b4faa933d647c82104cbaf1de2b435df1ee637cf
CryRtex <itspecialistt@protonmail.com>
2023-01-29 20:27:52 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -72,17 +72,9 @@ export default function Navbar({ items }: NavbarProps) {
             <Logo />
           </LogoWrapper>
         </NextLink>
-        <NavItemList>
-          {items.map((singleItem) => (
-            <NavItem key={singleItem.href} {...singleItem} />
-          ))}
-        </NavItemList>
         <ColorSwitcherContainer>
           <ColorSwitcher />
         </ColorSwitcherContainer>
-        <HamburgerMenuWrapper>
-          <HamburgerIcon aria-label="Toggle menu" onClick={toggle} />
-        </HamburgerMenuWrapper>
       </Content>
     </NavbarContainer>
   );
